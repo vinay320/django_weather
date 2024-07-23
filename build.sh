f27a695368e26864d5a12dd1b302fadad2edcded
Vinay <vinaytomar939@gmail.com>
2024-07-23 12:25:30 +0530

    main code changes

--- build.sh
+++ build.sh
@@ -3,11 +3,11 @@
 # Build the project
 
 echo "Building the project..."
-python3.12 -m pip install -r requirements.txt
+python -m pip install -r requirements.txt
 
 echo "Making Migrations..."
-python3.12 manage.py makemigrations --noinput
-python3.12 manage.py migrate --noinput
+python manage.py makemigrations --noinput
+python manage.py migrate --noinput
 
 echo "Collecting Static Files..."
-python3.12 manage.py collectstatic --noinput --clear
+python manage.py collectstatic --noinput --clear
